784e5f8d7207a94c8669b6a2ec79b8d615762bd6
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-06-20 14:22:29 +0300

    first demo html css js

---
+++ class-01/demo/js/app.js
@@ -0,0 +1,40 @@
+// alert('hi')
+//string
+// var userName = prompt('what is your name');
+// console.log(userName);
+// var age = 35;//number
+// var grade = 75.5
+// var result = true;//boolean
+
+var userAge = 35;
+var userAge = 25;
+console.log(userAge);
+
+
+grade = 100;
+var grade;
+console.log(grade);
+
+
+let userName = 'shihab';
+userName = 'ahmad';
+console.log(userName);
+
+// mark = 90;
+// let mark;
+// console.log(mark);
+
+const pie = 3.14;
+
+// pie = 3;
+
+console.log(pie);
+
+let str1 = 'shihab ';
+let str2 = 'eshtaiwi';
+console.log(str1 + str2);
+
+let num1 = 10;
+let num2 = '100';
+let num3 = num1 + num2;
+console.log(typeof num3);
