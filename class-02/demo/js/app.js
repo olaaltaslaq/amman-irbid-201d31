145b7de8ec490b7874078c822164763d220550af
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-06-21 11:38:27 +0300

    demo

---
+++ class-02/demo/js/app.js
@@ -0,0 +1,138 @@
+'use strict';
+// let num1 = parseInt(prompt('enter first number'));
+// console.log('num1', num1);
+// let num2 = Number(prompt('enter second number'));
+// console.log('num2', num2);
+// let num3 = parseInt(prompt('enter third number'));
+// console.log('num3', num3);
+
+// console.log('sum = ', num1 + num2 + num3);
+
+// let q1 = confirm('do you like coding?');
+// console.log(q1);
+
+// let answer1 = Number(prompt('please enter a number'));
+//3 
+//'3' === 5 >>> f
+//5
+//'5' === 5 >>> f
+// if (answer1 == 5) {
+//     console.log('correct');
+// }
+
+// if (answer1 == 5) {
+//     //code
+//     console.log('correct');
+// }
+// else {
+//     //another code
+//     console.log('wrong');
+// }
+
+
+// if (answer1 == 5) {
+//     //code
+//     console.log('correct');
+// } else if (answer1 == 3) {
+//     //another code
+//     console.log('correct');
+// } else if (answer1 == 4) {
+//     console.log('correct');
+// } else {
+//     console.log('wrong');
+// }
+/*
+if (condition) {
+    //code
+}
+
+if (condition) {
+    //code
+}
+else {
+    //another code
+}
+
+
+if (condition) {
+    //code
+} else if (another condition) {
+    //another code
+}else if ( ..) {
+
+} else if (..) {
+
+}
+*/
+//switch
+// switch (answer1) {
+//     case 5:
+//         console.log('correct');
+//         break;
+//     case 4:
+//         console.log('correct');
+//         break;
+//     default:
+//         console.log('wrong');
+//         break;
+// }
+
+// let userType = prompt('please enter your type (user/admin)?');
+// switch (userType) {
+//     case 'user':
+//         console.log('sorry you can\'t access');
+//         break;
+//     case 'admin':
+//         console.log('ok you can access');
+//         break;
+//     default:
+//         console.log('sorry we don\'t have this user');
+//         break;
+// }
+
+// let carType = prompt('enter your car type').toUpperCase();
+
+// switch (carType) {
+//     case 'KIA':
+//     case 'LADA':
+//         console.log('nice car');
+//         break;
+//     case 'BMW':
+//         console.log('very nice car');
+//         break;
+//     default:
+//         console.log('why you don\'t have one');
+//         break;
+// }
+
+let num1 = Number(prompt('enter num1'));
+let num2 = Number(prompt('enter num2'));
+
+if (num1 >= num2) {
+    console.log('num1 is greater than num2');
+} else if (num1 < num2) {
+    console.log('num1 is smaller than num2');
+}
+// 3 <= 3
+
+// && || !
+
+//   leftside  &&  rightside
+// T && T >>>> T
+// F && T >>>> F
+// T && F >>>> F
+// F && F >>>> F
+// if(num1 < num2 && num2 <num3 )
+// {
+
+// }
+
+//   leftside   ||  rightside
+// T || T >>>> T
+// F || T >>>> T
+// T || F >>>> T
+// F || F >>>> F
+
+// !
+//T! >>>> F
+//F! >>>> T  
