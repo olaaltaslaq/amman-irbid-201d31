34ca12b3ea7350a1ad06846951f1bb890691d1e0
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-06-24 11:50:17 +0300

    demos

---
+++ class-05/demo/app.js
@@ -0,0 +1,20 @@
+let fun1 = function () {
+    let userName = 'shihab';
+    return userName;
+    // console.log();
+}
+console.log(fun1());
+fun1();
+
+
+let age = 35;
+
+fun2();
+function fun2() {
+    age = 36;
+}
+
+(function (user) {
+
+})('areej')
+
