572008d55ee012820f6cfb6c7a195ecca40c7a40
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-06-26 16:00:38 +0300

    demo

---
+++ class-06/demo/js/app.js
@@ -0,0 +1,29 @@
+// problem domain: the Seattle Kitten Rescue has tons of kittens who need good homes. One of the best ways to reach prospective adoptive homes is to have profiles for each kitten available on a website. There are hundreds of kittens, though, and only a few volunteers; it's too time-consuming to hand-code each kitten's profile on their website. They need a better way.
+
+// Each kitten's profile should have:
+// - name
+// - random age assigned
+// - a list of what they like
+// - an image
+// good with kids
+// good with dogs
+// good with other cats
+// breed
+
+// TODO: dynamically generate kitten objects 
+
+'use strict';
+
+
+// object literals
+// bracket and dot notation
+// math.random
+// add kittens to the page
+// each kitten gets an Article
+// each kitten's name shown as a subhead
+// each pic shown as an image with dynamically-created filename (involves setting attributes)
+// each array of interests shown as a list
+// getElementById
+// createElement
+// createTextNode
+// appendChild
